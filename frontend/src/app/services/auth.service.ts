cf7951ccf8b2da29a399ebc711057534ca0faf76
lgruneirodem <lgruneirodem@gmail.com>
2024-07-09 21:01:50 +0200

    auth service

--- frontend/src/app/services/auth.service.ts
+++ frontend/src/app/services/auth.service.ts
@@ -1,14 +1,16 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from '../../environments/environment';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  private apiUrl = 'http://localhost'; // Cambia esto por la URL de tu API
 
+  private apiUrl = environment.rutaApi; 
   constructor(private http: HttpClient) { }
 
   login(email: string, password: string): Observable<any> {
@@ -23,8 +25,8 @@ export class AuthService {
       );
   }
 
-  register(user: any, email: string, usuario: string, password: string, password_confirmation: string): Observable<any> {
-    return this.http.post<any>(`${this.apiUrl}/register`, user)
+  register(nombre: string, email: string, user: string, password: string, esAdmin: string): Observable<any> {
+    return this.http.post<any>(`${this.apiUrl}/register`, email)
       .pipe(
         map(response => {
           if (response.token) {
@@ -33,20 +35,5 @@ export class AuthService {
           return response;
         })
       );
-  }
-
-  getAuthenticatedUser(): Observable<any> {
-    const token = localStorage.getItem('token');
-    const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
-
-    return this.http.get<any>(`${this.apiUrl}/user`, { headers });
-  }
-
-  logout(): void {
-    localStorage.removeItem('token');
-  }
-
-  isLoggedIn(): boolean {
-    return !!localStorage.getItem('token');
   }
 }
